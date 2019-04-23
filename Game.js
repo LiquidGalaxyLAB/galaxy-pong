5103b48011cc97aa7ec33d4635e6d9fd00827055
Augusto Shimizu <augustohss@gmail.com>
2019-04-23 10:25:23 -0300

    Game logic pt 2

--- Game.js
+++ Game.js
@@ -12,9 +12,15 @@ var DIRECTION = {
 
 //Ball object
 var Ball = {
-    new: function(){
-        return  
-        //blablabla
+    new: function(speed){
+		this.width = 50;
+		this.height = 50;
+		this.x = 0;
+		this.y = 0;
+		this.moveX = DIRECTION.IDLE;
+		this.moveY = DIRECTION.IDLE; 
+		this.speed = speed || 10;
+        return 
     }
 }
 
@@ -51,10 +57,85 @@ var Game = {
     },
     update: function(){
         //update 
+		if(!this.over)
+		{
+			//Ball movement
+			if (this.ball.x <= 0) 
+			{
+				this.player2.score+=1;
+				this.ball.x = this.canvas.width/2;
+				this.ball.y = this.canvas.height/2;
+				ball.moveX = DIRECTION.LEFT;
+			}
+			if (this.ball.x >= this.canvas.width - this.ball.width)
+			{
+				this.player1.score+=1;
+				this.ball.x = this.canvas.width/2;
+				this.ball.y = this.canvas.height/2;
+				ball.moveX = DIRECTION.RIGHT;
+			}
+			
+			if (this.ball.y <= 0) this.ball.moveY = DIRECTION.DOWN;
+			if (this.ball.y >= this.canvas.height - this.ball.height) this.ball.moveY = DIRECTION.UP;
+			
+			//Move ball
+			if (this.ball.moveY === DIRECTION.UP) this.ball.y -= (this.ball.speed / 1.5);
+			else if (this.ball.moveY === DIRECTION.DOWN) this.ball.y += (this.ball.speed / 1.5);
+			if (this.ball.moveX === DIRECTION.LEFT) this.ball.x -= this.ball.speed;
+			else if (this.ball.moveX === DIRECTION.RIGHT) this.ball.x += this.ball.speed;
+			
+			//Player movement
+			if (this.player1.move === DIRECTION.UP) this.player.y -= this.player.speed;
+			else if (this.player1.move === DIRECTION.DOWN) this.player.y += this.player.speed;
+			
+			if (this.player2.move === DIRECTION.UP) this.player.y -= this.player.speed;
+			else if (this.player2.move === DIRECTION.DOWN) this.player.y += this.player.speed;
+			
+			//Player movement limit
+			if (this.player1.y <= 0) this.player1.y = 0;
+			else if (this.player1.y >= (this.canvas.height - this.player1.height)) this.player1.y = (this.canvas.height - this.player1.height);
+			if (this.player2.y <= 0) this.player2.y = 0;
+			else if (this.player2.y >= (this.canvas.height - this.player2.height)) this.player2.y = (this.canvas.height - this.player2.height);
+			
+			//Player1 Collision
+			if (this.ball.x - this.ball.width <= this.player1.x && this.ball.x >= this.player1.x - this.player1.width) {
+				if (this.ball.y <= this.player1.y + this.player1.height && this.ball.y + this.ball.height >= this.player1.y) {
+					this.ball.x = (this.player1.x + this.ball.width);
+					this.ball.moveX = DIRECTION.RIGHT;
+				}
+			}
+			
+			//Player2 Collision
+			if (this.ball.x - this.ball.width <= this.player2.x && this.ball.x >= this.player2.x - this.player2.width) {
+				if (this.ball.y <= this.player2.y + this.player2.height && this.ball.y + this.ball.height >= this.player2.y) {
+					this.ball.x = (this.player2.x + this.ball.width);
+					this.ball.moveX = DIRECTION.RIGHT;
+				}
+			}
+			
+			if(this.player1.score == 9 || this.player2.score == 9)
+			{
+				this.running = false;
+				this.over = true;
+			}
+		}
         
     },
     draw: function(){
-        //draw the objects 
+		//draw the objects 
+		this.context.clearRect(0, 0, canvas.width, canvas.height);
+		this.context.fillStyle = '000000';
+		this.context.fillRect(0, 0, canvas.width, canvas.height);
+		this.context.fillStyle = 'ffffff';
+		this.context.fillRect(this.player1.x, this.player1.y, this.player1.width, this.player1.height);
+		this.context.fillRect(this.player2.x, this.player2.y, this.player2.width, this.player2.height);
+		this.context.fillRect(this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+		
+		for(var i=0; i<iniH ; i+= 25)
+			c.fillRect(canvas.width/2, i, 25, 25);
+		
+		drawNum(pontoP1, canvas.width/2 - 3*25, 25, 15);
+		drawNum(pontoP2, canvas.width/2 + 2*25, 25, 15);
     },
     loop: function(){
         Game.update();
@@ -92,4 +173,103 @@ var Game = {
             Game.player1.move = DIRECTION.IDLE
             Game.player2.move = DIRECTION.IDLE});
     }
+	
+	drawNum: function(num, x, y, tam){
+		switch(num){
+		case 0:
+			for(var i=y; i < y+5*tam; i+=tam)
+			{
+				c.fillRect(x, i, tam, tam);
+				c.fillRect(x+2*tam, i, tam, tam);
+			}
+			c.fillRect(x+tam, y, tam, tam);
+			c.fillRect(x+tam, y+4*tam, tam, tam);
+			break;
+		case 1:
+			for(var i=y; i < y+5*tam; i+=tam)
+				c.fillRect(x+2*tam, i, tam, tam);
+			c.fillRect(x+tam, y, tam, tam);
+			break;
+		case 2:
+			for(var i=x; i < x+3*tam; i+=tam)
+			{
+				c.fillRect(i, y, tam, tam);
+				c.fillRect(i, y+2*tam, tam, tam);
+				c.fillRect(i, y+4*tam, tam, tam);
+			}
+			c.fillRect(x+2*tam, y+tam, tam, tam);
+			c.fillRect(x,y+3*tam, tam, tam);
+			break;
+		case 3:
+			for(var i=x; i < x+3*tam; i+=tam)
+			{
+				c.fillRect(i, y, tam, tam);
+				c.fillRect(i, y+2*tam, tam, tam);
+				c.fillRect(i, y+4*tam, tam, tam);
+			}
+			c.fillRect(x+2*tam, y+tam, tam, tam);
+			c.fillRect(x+2*tam,y+3*tam, tam, tam);
+			break;
+		case 4:
+			for(var i=y; i < y+5*tam; i+=tam)
+				c.fillRect(x+2*tam, i, tam, tam);
+			for(var i=y; i < y+3*tam; i+=tam)
+				c.fillRect(x, i, tam, tam);
+			c.fillRect(x+tam, y+2*tam, tam, tam);
+			break;
+		case 5:
+			for(var i=x; i < x+3*tam; i+=tam)
+			{
+				c.fillRect(i, y, tam, tam);
+				c.fillRect(i, y+2*tam, tam, tam);
+				c.fillRect(i, y+4*tam, tam, tam);
+			}
+			c.fillRect(x, y+tam, tam, tam);
+			c.fillRect(x+2*tam,y+3*tam, tam, tam);
+			break;
+		case 6:
+			for(var i=x; i < x+3*tam; i+=tam)
+			{
+				c.fillRect(i, y, tam, tam);
+				c.fillRect(i, y+2*tam, tam, tam);
+				c.fillRect(i, y+4*tam, tam, tam);
+			}
+			c.fillRect(x, y+tam, tam, tam);
+			c.fillRect(x, y+3*tam, tam, tam);
+			c.fillRect(x+2*tam,y+3*tam, tam, tam);
+			break;
+		case 7:
+			for(var i=x; i < x+3*tam; i+=tam)
+				c.fillRect(i, y, tam, tam);
+			c.fillRect(x+2*tam, y+tam, tam, tam);
+			for(var i=y+2*tam; i < y+5*tam; i+=tam)
+				c.fillRect(x+tam, i, tam, tam);
+			break;
+		case 8:
+			for(var i=x; i < x+3*tam; i+=tam)
+			{
+				c.fillRect(i, y, tam, tam);
+				c.fillRect(i, y+2*tam, tam, tam);
+				c.fillRect(i, y+4*tam, tam, tam);
+			}
+			c.fillRect(x, y+tam, tam, tam);
+			c.fillRect(x+2*tam, y+tam, tam, tam);
+			c.fillRect(x, y+3*tam, tam, tam);
+			c.fillRect(x+2*tam, y+3*tam, tam, tam);
+			
+			break;
+		case 9:
+			for(var i=x; i < x+3*tam; i+=tam)
+			{
+				c.fillRect(i, y, tam, tam);
+				c.fillRect(i, y+2*tam, tam, tam);
+				c.fillRect(i, y+4*tam, tam, tam);
+			}
+			c.fillRect(x, y+tam, tam, tam);
+			c.fillRect(x+2*tam, y+tam, tam, tam);
+			c.fillRect(x+2*tam, y+3*tam, tam, tam);
+			break;
+		}
+	
+	}
 }
