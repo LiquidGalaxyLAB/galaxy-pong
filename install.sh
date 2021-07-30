deede1dc645a5eefc5a85aa996fd0ba6dbf12673
leoruas <leo.coelho.ruas@gmail.com>
2021-07-29 21:31:58 -0300

    updated installer

--- install.sh
+++ install.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
 
-echo "Installing Galaxy Pong" >$HOME/installer/projLog/pong.txt
+#Create logs directory if it doesnt exist yet
+mkdir -p ./logs
 
-# Initialize sudo access
+#Create file name with name as date
+date=$(date +%m-%d-%y)
+filename="$date.txt"
 
-sudo -v
+# Add to log with timestamp
+time=$(date +%H:%M:%S)
+echo "[$time] Installing Galaxy Pong..." | tee -a ./logs/$filename
 
 # Open port 8112
 
-LINE=`cat /etc/iptables.conf | grep "tcp" | grep " 8100" | awk -F " -j" '{print $1}'`
+LINE=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | awk -F " -j" '{print $1}'`
 
 RESULT=$LINE",8112"
 
-DATA=`cat /etc/iptables.conf | grep "tcp" | grep " 8100" | grep "8112"`
+DATA=`cat /etc/iptables.conf | grep "tcp" | grep "8111" | grep "8112"`
 
 if [ "$DATA" == "" ]; then
-    sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>>$HOME/installer/projLog/pong.txt
+    time=$(date +%H:%M:%S)
+    echo "[$time] Port 8112 not open, opening port..." | tee -a ./logs/$filename
+    sudo sed -i "s/$LINE/$RESULT/g" /etc/iptables.conf 2>> ./logs/$filename
 else
-    echo "Port already open"
+    time=$(date +%H:%M:%S)
+    echo "[$time] Port already open." | tee -a ./logs/$filename
 fi
 
-# Install all dependencies
+# Install dependencies
+time=$(date +%H:%M:%S)
+echo "[$time] Installing dependencies..." | tee -a ./logs/$filename
+npm install 2>> ./logs/$filename
 
-npm install
+# Stop server if already started
+pm2 delete PONG_PORT:8112 2> /dev/null
 
-# Server
+# Start server
+time=$(date +%H:%M:%S)
+echo "[$time] Starting pm2..." | tee -a ./logs/$filename
+pm2 start index.js --name PONG_PORT:8112 2>> ./logs/$filename
 
-pm2 start index.js --name PONG_PORT:8112 2>>$HOME/installer/projLog/pong.txt
+pm2 save 2>> ./logs/$filename
 
-pm2 save 2>>$HOME/installer/projLog/pong.txt
+time=$(date +%H:%M:%S)
+echo "[$time] Installation complete. Reboot machine to finish installation" | tee -a ./logs/$filename
 
-echo "Installation complete" >>$HOME/installer/projLog/pong.txt
+read -p "Do you want to reboot your machine now? [Y/n]: " yes
+
+if [[ $yes =~ ^[Yy]$ ]]
+then
+  reboot
+fi
