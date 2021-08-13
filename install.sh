b5bdb2a847a636a3259d1b731c55860eb8a4286e
leoruas <leo.coelho.ruas@gmail.com>
2021-08-13 09:49:17 -0300

    updated installer

--- install.sh
+++ install.sh
@@ -33,20 +33,23 @@ time=$(date +%H:%M:%S)
 echo "[$time] Installing dependencies..." | tee -a ./logs/$filename
 npm install 2>> ./logs/$filename
 
+# Add access for pm2
+sudo chown lg:lg /home/lg/.pm2/rpc.sock /home/lg/.pm2/pub.sock
+
 # Stop server if already started
-sudo pm2 delete PONG_PORT:8112 2> /dev/null
+pm2 delete PONG_PORT:8112 2> /dev/null
 
 # Start server
 time=$(date +%H:%M:%S)
 echo "[$time] Starting pm2..." | tee -a ./logs/$filename
-sudo pm2 start index.js --name PONG_PORT:8112 2>> ./logs/$filename
+pm2 start index.js --name PONG_PORT:8112 2>> ./logs/$filename
 
-sudo pm2 save 2>> ./logs/$filename
+pm2 save 2>> ./logs/$filename
 
 # Add automatic pm2 resurrect script
 time=$(date +%H:%M:%S)
 echo "[$time] Updating resurrect script..." | tee -a ./logs/$filename
-RESURRECT=pm2 startup | grep 'sudo'
+RESURRECT=$(pm2 startup | grep 'sudo')
 eval $RESURRECT 2>> ./logs/$filename
 
 time=$(date +%H:%M:%S)
